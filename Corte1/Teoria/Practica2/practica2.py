c2202007e16852f32e528c0317b8615479cca9a6
MateoRodriguez14 <mateo.142616@gmail.com>
2026-02-04 21:04:24 -0500

    Se agrega la tarea de imprimir la información del tipo de variable en un solo renglón

--- Corte1/Teoria/Practica2/practica2.py
+++ Corte1/Teoria/Practica2/practica2.py
@@ -1,12 +1,20 @@
 n1 = 3
 n2 = 3.0
 n3 = "3"
-print()
+
+'''Ejemplo de impresión de datos visto en clase'''
 print("n1 es: ")
-print(type(n1))
-print()
+print(type(n1),'\n')
 print("n2 es: ")
-print(type(n2))
-print()
+print(type(n2),'\n')
 print("n3 es: ")
-print(type(n3))
+print(type(n3),'\n')
+print("--/--/--/--/--/--/--/--/\n")
+
+'''Solución Tarea formas de imprimir en consola en python'''
+
+print(f'n1 es: {type(n1)}')
+print(f'n2 es: {type(n2)}')
+print(f'n3 es: {type(n3)}')
+
+
