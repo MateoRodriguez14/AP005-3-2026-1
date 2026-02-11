5def76dd088e0b8e82e3e4dc7a12ba7e6d285588
MateoRodriguez14 <89656841+MateoRodriguez14@users.noreply.github.com>
2026-02-11 15:37:35 -0500

    Se sube la practica 2 de c++ de sumar dos digitos

---
+++ Corte1/teoriaCpp/Practica2/practica2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int numeroUno = 0;
+	int numeroDos = 0;
+	int resultadoSumaNumeros = 0;
+	cout<<"Bienvenid@ a la calculadora de dos numeros"<<endl;
+	cout<<"Ingrese el primer digito: "<<endl;
+	cin >> numeroUno;
+	cout<<"Ingrese el segundo digito: "<<endl;
+	cin >> numeroDos;
+	resultadoSumaNumeros = numeroUno+numeroDos;
+	cout <<"El resultado de la suma es: "<<resultadoSumaNumeros<<endl;
+	return 0;
+}
