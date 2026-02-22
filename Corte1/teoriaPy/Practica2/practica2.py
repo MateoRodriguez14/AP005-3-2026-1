272a22545d8a547f3d5fd752287c33d8cbccbf82
MateoRodriguez14 <mateo.142616@gmail.com>
2026-02-21 23:01:35 -0500

    Se ha cambiado el nombre de la carpeta Teoria a teoriaPy y se ha agregado archivo python de listas y tuplas

---
+++ Corte1/teoriaPy/Practica2/practica2.py
@@ -0,0 +1,28 @@
+n1 = 3
+n2 = 3.0
+n3 = "3"
+
+'''Ejemplo de impresión de datos visto en clase'''
+print("n1 es: ")
+print(type(n1),'\n')
+print("n2 es: ")
+print(type(n2),'\n')
+print("n3 es: ")
+print(type(n3),'\n')
+print("--/--/--/--/--/--/--/--/\n")
+
+'''Solución Tarea formas de imprimir en una sola línea en consola en python'''
+
+'''Primera forma de impresión en  una sola línea'''
+print("Primera forma de impresion en  una sola linea\n")
+print(f'n1 es: {type(n1)}')
+print(f'n2 es: {type(n2)}')
+print(f'n3 es: {type(n3)}\n')
+
+'''Segunda forma de impresión en  una sola línea'''
+print("Segunda forma de impresion en  una sola linea\n")
+print('n1 es:',type(n1))
+print('n2 es:',type(n2))
+print('n3 es:',type(n3))
+
+
---
+++ Corte1/teoriaPy/Practica3/Clase10Febrero.py
@@ -0,0 +1,10 @@
+a = 8
+print("Estación 1")
+
+if(a == 8):
+	print("Dentro del condicional")
+	print("El valor de a si es 8")
+	
+else:
+	print("Dentro del else")
+	print("El valor de a no es 8")
